086909791d2f4fd435e81b2c79eee20538a5d435
Alex Tangen <atangen.at@gmail.com>
2022-11-30 20:38:08 -0600

    begin crawler updates to continue later

--- walker/walker/spiders/web_walker.py
+++ walker/walker/spiders/web_walker.py
@@ -1,11 +1,19 @@
 import scrapy
-
+from bs4 import BeautifulSoup
+import requests
 
 class WebWalkerSpider(scrapy.Spider):
     name = 'web_walker'
     start_urls = ['http://www.geeksforgeeks.org/']
     follow = True
-    # start_urls = ['https://www.chevrolet.com/performance/corvette']
+
+    def parser(site_to_scrape):
+        site = requests.get(site_to_scrape)
+
+        site_text = BeautifulSoup(site.text, "html.parser")
+
+        for div in site_text.findall("a"):
+
 
     def parse(self, response):
         pass
